ee817d7e50da9a5729c5ba637269a25f02f6867a
Cpzty <Cristianpzty@gmail.com>
2018-08-26 17:14:02 -0600

    ...

--- src/index.js
+++ src/index.js
@@ -10,12 +10,18 @@ import {Provider,connect} from 'react-redux';
 import addProduct from './Actions/ProductActions';
 import addTotal from './Actions/UserActions';
 
+const Product = ({name,price,quantity})=>(
+<div >
+  {name}-Q:{price}-Count:{quantity}
+<button>Add to cart</button>
+  </div>
+);
 
 const Productsreducer = (state=[],action)=>{
   switch(action.type){
     case 'PRODUCT_ADDED':
-      return [...state,action.payload]
-             
+      return [...state,action.payload];
+    
     default: return state;
   }
   
@@ -48,5 +54,42 @@ console.log(store.getState());
 store.dispatch(addTotal(20));
 console.log(store.getState());
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const ProductApp = ({text})=>(
+<div>
+  <ul>
+  <li>
+  <Product
+  name="bananas"
+  price="50"
+  quantity="10"
+  />
+  </li>
+  <h1/>
+  <li>
+  <Product
+  name="apples"
+  price="10"
+  quantity="5"
+  />
+  </li>
+  
+  
+  </ul>
+  <div>
+  <input type="text" placeholder="name"/>
+  <input type="text" placeholder="price"/>
+  <input type="text" placeholder="quantity"/>
+  <button>Add new product</button>
+  </div>
+  <div>
+  <p>Total:0</p>
+  </div>
+  </div>
+);
+
+ReactDOM.render(
+   <Provider store={store}>
+  <ProductApp/>
+  </Provider>
+  , document.getElementById('root'));
 
