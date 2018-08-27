e6ea61eb3f250061b934508be1c26c1cafcc86e0
Cpzty <Cristianpzty@gmail.com>
2018-08-26 22:51:09 -0600

    arreglado cartpage direccionando a homepage y contador

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage';
-import CartPage from './pages/homepage';
+import CartPage from './pages/cartpage';
 const Router = () => (
   <Switch>
   {/*ruta por default con forward slash*/}
--- src/features/product-listing/product-list-item.js
+++ src/features/product-listing/product-list-item.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 
 export default function ProductListItem(props){
+  const thisItemInCart = props.cart.filter(item=>item.id===props.product.id)[0];
   return <div className="product-list-item">
     <h3>{props.name}</h3>
     <img 
@@ -12,7 +13,9 @@ export default function ProductListItem(props){
     <div>{ props.product.description }</div>
     <div>${ props.product.price }</div>
     <div>
-      <button>Add to cart</button> 
+      <button onClick={()=>props.addToCart(props.product)}>Add to cart ({
+      (thisItemInCart && thisItemInCart.quantity) || 0
+      })</button> 
     </div>
   </div>
 }
